ef18b07b90ceeda7e3f044f7a9e32a0f8276dfef
Leonardo Tenshi <92759555+leoTenshi@users.noreply.github.com>
2025-12-01 17:40:13 -0300

    Add files via upload

--- service/TarefaService.php
+++ service/TarefaService.php
@@ -4,63 +4,39 @@ class TarefaService {
 
     private TarefaDAO $dao;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->dao = new TarefaDAO();
     }
 
-    public function listar()
-    {
-        try {
-            return $this->dao->listar();
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage());
-        }
+    public function listar() {
+        return $this->dao->listar(); 
     }
 
-    public function inserir(array $dados)
-    {
-        try {
-            $dados['status'] = 'pendente';
-            $resultado = $this->dao->inserir($dados); 
+    public function inserir(array $dados) {
+        $dados['status'] = 'pendente';
+        $resultado = $this->dao->inserir($dados); 
 
-            if (!$resultado) {
-                throw new Exception("Falha ao inserir a tarefa. Verifique os dados fornecidos.", 400);
-            }
-            return $resultado; // Retorna o resultado da DAO 
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage(), (int)$e->getCode());
+        if (!$resultado) {
+            throw new Exception("Falha ao inserir a tarefa.", 400);
         }
+        return $resultado;
     }
 
-    public function atualizarStatus($id, $status)
-    {
-        try {
-            $resultado = $this->dao->atualizarStatus($id, $status);
+    public function atualizarStatus($id, $status) {
+        $resultado = $this->dao->atualizarStatus($id, $status);
 
-            if (!$resultado) {
-                // Se rowCount() for 0, lança exceção
-                throw new Exception("Tarefa não encontrada.", 404);
-            }
-
-            return true;
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage(), (int)$e->getCode());
+        if (!$resultado) {
+            throw new Exception("Tarefa não encontrada.", 404);
         }
+        return true;
     }
 
-    public function excluir($id)
-    {
-        try {
-            $resultado = $this->dao->excluir($id);
+    public function excluir($id) {
+        $resultado = $this->dao->excluir($id);
 
-            if (!$resultado) {
-                // Se rowCount() for 0, lança exceção
-                throw new Exception("Tarefa não encontrada.", 404);
-            }
-            return true;
-        } catch (Exception $e) {
-            throw new Exception($e->getMessage(), (int)$e->getCode());
+        if (!$resultado) {
+            throw new Exception("Tarefa não encontrada.", 404);
         }
+        return true;
     }
 }
